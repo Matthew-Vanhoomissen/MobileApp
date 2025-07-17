8993c32079d04fb5c4ceed7d55ff3687f1b3468a
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-07-16 21:35:26 -0700

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,21 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text>A New and Updated</Text>
+      <Text>Alarm and Clock App</Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
