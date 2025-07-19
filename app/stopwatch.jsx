a891e34a33a4d0d8f8fa158e149c7746534b221e
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-07-18 18:01:04 -0700

    more files

---
+++ app/stopwatch.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Stopwatch = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default Stopwatch
+
+const styles = StyleSheet.create({})
