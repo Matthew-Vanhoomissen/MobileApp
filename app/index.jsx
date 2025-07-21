e12595166f28b99295637e793859d16c78989f6e
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-07-21 01:07:56 -0700

    images

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Image, View } from 'react-native'
 import {Link} from 'expo-router'
-
+import WClock from '../assets/img/wclock.png'
 //Themed components
 import ThemedView from '../components/ThemedView'
 import ThemedText from '../components/ThemedText'
@@ -8,13 +8,22 @@ import ThemedText from '../components/ThemedText'
 const Home = () => {
   return (
     <ThemedView style={styles.container}>
+    
       <ThemedText>A New and Updated</ThemedText>
       <ThemedText>Alarm and Clock App</ThemedText>
 
+    <ThemedView style={[{backgroundColor: 'blue', }]}>
       <Link href="/timer">timer</Link>
-      <Link href="/clock">clock</Link>
+      <Link href="/clock"><Image source={WClock}/></Link>
       <Link href="/stopwatch">stopwatch</Link>
     </ThemedView>
+      
+      
+      
+    
+    </ThemedView>
+    
+    
   )
 }
 
