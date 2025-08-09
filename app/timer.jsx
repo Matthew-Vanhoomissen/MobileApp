c07612539372ef67461994df4308a4d49ce45ce5
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-08-08 20:24:26 -0700

    timer1

--- app/timer.jsx
+++ app/timer.jsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import {Link} from 'expo-router'
+import { useState, useRef } from 'react'
 
 import WClock from '../assets/img/wclock.png'
 import Alarm from '../assets/img/alarm.png'
@@ -11,7 +12,56 @@ import TTimer from '../assets/img/timer.png'
 import ThemedView from '../components/ThemedView'
 import ThemedText from '../components/ThemedText'
 
+
+
 const Timer = () => {
+    //elapsed time
+    const [time, setTime] = useState(1000);
+    
+    //boolean if running
+    const [running, setRunning] = useState(false);
+    //store if the timer has run 
+    const [hasRun, setHasRun] = useState(false);
+    //store interval
+    const interval = useRef(null);
+    //store begin time
+    const startTime = useRef(0);
+
+    
+
+  const startTimer = () => {
+    const currentTime = time;
+    startTime.current = Date.now()
+
+    interval.current = setInterval(() => {
+      const elapsed = (Date.now() - startTime.current) / 1000;
+      setTime(currentTime - elapsed);
+    }, 1000);
+
+    setRunning(true);
+    setHasRun(true);
+  };
+
+  const pauseTimer = () => {
+    clearInterval(interval.current);
+
+    setRunning(false);
+  };
+
+  const cancelTimer = () => {
+    clearInterval(interval.current);
+
+    setTime(1000);
+    
+    setRunning(false);
+    setHasRun(false);
+  };
+
+  const secs = Math.floor(time);
+  const mins = Math.floor(time/60);
+    
+
+
   return (
     <ThemedView style={styles.container}>
 
@@ -19,7 +69,11 @@ const Timer = () => {
       <ThemedText style={[{color: 'black', fontSize: 18}]}>Timer</ThemedText>
       
     </ThemedView>
-
+    <ThemedView style={styles.timer}>
+      <ThemedText>{secs}</ThemedText>
+      <TouchableOpacity onPress={startTimer}><ThemedText>Start</ThemedText></TouchableOpacity>
+      <TouchableOpacity><ThemedText>Pause</ThemedText></TouchableOpacity>
+    </ThemedView>
     <ThemedView style={[{position: 'absolute', height:'10%', width: '100%', bottom:0, backgroundColor: 'white', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}]}>
 
     
@@ -68,4 +122,9 @@ const styles = StyleSheet.create({
     flex: 1,
     
   },
+  timer: {
+    flex: 1,
+    top: '50%',
+    left: '50%',
+  }
 })
