8501eac4489a68b03c58d5e300ba8612e1b18db0
Matthew-Vanhoomissen <ma.vanhoom1@gmail.com>
2025-09-25 00:07:35 -0700

    Visuals set up to add an alarm

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, View, TouchableOpacity, Modal, TouchableHighlight, ScrollView} from 'react-native'
+import { Switch, StyleSheet, Image, View, TouchableOpacity, Modal, TouchableHighlight, ScrollView, TextInput} from 'react-native'
 import {Link} from 'expo-router'
 import {useState, useRef} from "react"
 import { GestureHandlerRootView } from "react-native-gesture-handler";
@@ -19,9 +19,44 @@ const Home = () => {
   const scrollRefM = useRef(null);
   const scrollRefH = useRef(null);
 
-    const numbers = [null, ...Array.from({ length: 60}, (_, i) => i), null];
-    const hrs = [null, ...Array.from({ length: 12}, (_, i) => i + 1), null];
-    const amPm = ["","AM", "PM", ""];
+  const numbers = [null, ...Array.from({ length: 60}, (_, i) => i), null];
+  const hrs = [null, ...Array.from({ length: 12}, (_, i) => i + 1), null];
+  const amPm = ["","AM", "PM", ""];
+
+  const [text, setText] = useState("");
+  const [isEnabled, setIsEnabled] = useState(false);
+  //const [time, setTime] = useState(0);
+  const [hours, setHours] = useState(1);
+  const [minutes, setMinutes] = useState(0);
+  const [period, setPeriod] = useState("AM");
+
+  const setScroll = (event) => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+    const index = Math.round(offsetY / 60); //60 is the item height
+    setPeriod(amPm[index + 1])
+  }
+  const setScrollM = (event) => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+    const index = Math.round(offsetY / 60);
+    setMinutes(numbers[index + 1])
+  }
+  const setScrollH = (event) => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+    const index = Math.round(offsetY / 60);
+    setHours(hrs[index + 1])
+  }
+  
+
+  const addAlarm = () => {
+    let offset = 0;
+    {period === "PM" ? offset = 12 : offset}
+    console.log(hours + offset);
+    console.log(minutes);
+    console.log(period);
+    setVisible(false);
+  }
+
+  
   return (
     <ThemedView style={styles.container}>
 
@@ -47,35 +82,38 @@ const Home = () => {
         visible={visible}
         onRequestClose={() => setVisible(false)}>
         
-          
+        
         <ThemedView style={styles.newPanel}>
-          <ThemedText> </ThemedText>
-          <TouchableOpacity onPress={() => setVisible(false) }><ThemedText >    Back</ThemedText></TouchableOpacity>
+          
+          <TouchableOpacity onPress={() => setVisible(false) }><ThemedText style={[{top: '15'}]}>    Back</ThemedText></TouchableOpacity>
+          <ThemedText>                                                                         </ThemedText>
+          <TouchableOpacity onPress={() => addAlarm() } ><ThemedText style={[{top: '15'}]}>Confirm</ThemedText></TouchableOpacity>
         </ThemedView>
 
         <ThemedView style={styles.alarm}>
       
-          <ScrollView ref={scrollRefH} snapToInterval={60} snapToAlignment="center"decelerationRate="fast" scrollEventThrottle={16} >
+          <ScrollView ref={scrollRefH} snapToInterval={60} snapToAlignment="center"decelerationRate="fast" scrollEventThrottle={16} onScroll={setScrollH}>
             {hrs.map((num, idx) => (
-            <ThemedView key={idx} style={[{height: 60, backgroundColor: '#555555'}]}>
+            <ThemedView key={idx} style={[{height: 60, backgroundColor: '#686464ff', width: '16', left: '20'}]}>
               <ThemedText>
-                {num}
+                  {num}
               </ThemedText>
             </ThemedView>
             ))}
           </ScrollView>
-          <ScrollView ref={scrollRefM} snapToInterval={60} snapToAlignment="center"decelerationRate="fast" scrollEventThrottle={16} >
+          <ThemedText style={[{fontSize: 30, right: '2', top: '46'}]}> : </ThemedText>
+          <ScrollView ref={scrollRefM} snapToInterval={60} snapToAlignment="center"decelerationRate="fast" scrollEventThrottle={16} onScroll={setScrollM}>
             {numbers.map((num, idx) => (
-            <ThemedView key={idx} style={[{height: 60,  backgroundColor: '#555555'}]}>
+            <ThemedView key={idx} style={[{height: 60,  backgroundColor: '#686464ff', width: '18',  left: '20'}]}>
               <ThemedText>
-                {num}
+                {num === null ? "" : num < 10 ? "0" + num : num}
               </ThemedText>
             </ThemedView>
             ))}
           </ScrollView>
-          <ScrollView ref={scrollRefT} snapToInterval={60} snapToAlignment="center"decelerationRate="fast" scrollEventThrottle={16} >
+          <ScrollView ref={scrollRefT} snapToInterval={60} snapToAlignment="center"decelerationRate="fast" scrollEventThrottle={16} onScroll={setScroll}>
             {amPm.map((str, idx) => (
-            <ThemedView key={idx} style={[{height: 60,  backgroundColor: '#555555'}]}>
+            <ThemedView key={idx} style={[{height: 60,  backgroundColor: '#686464ff', width: '25',  left: '10'}]}>
               <ThemedText>
                 {str}
               </ThemedText>
@@ -83,15 +121,15 @@ const Home = () => {
             </ThemedView>
             ))}
           </ScrollView>
-          <ThemedView style={styles.label1}>
-            <ThemedText>hours</ThemedText>
+          
+        </ThemedView>
+        <ThemedView style={styles.buttonContainer}>
+          <ThemedView style={[{flexDirection: 'row', backgroundColor: '#686464ff', borderBottomColor: '#555555', borderBottomWidth: 1, height: '40'}]}>
+            <ThemedText style={[{top: '5'}]}>Repeating    </ThemedText>
+            <Switch value={isEnabled} onValueChange={setIsEnabled} trackColor={{ false: "#767577", true: "#f4f3f4" }}
+        thumbColor={isEnabled ? "#767577" : "#f4f3f4"} />
           </ThemedView>
-          <ThemedView style={styles.label2}>
-            <ThemedText>mins</ThemedText>
-          </ThemedView>
-          <ThemedView style={styles.label3}>
-            <ThemedText>secs</ThemedText>
-          </ThemedView>
+            <TextInput placeholder='Alarm Label: ' value={text} onChangeText={setText}/>
         </ThemedView>
         
       </Modal>
@@ -161,6 +199,7 @@ const styles = StyleSheet.create({
       backgroundColor: '#555555',
       height: '617',
       justifyContent: 'flex-start',
+      flexDirection: 'row'
     },
     label1: {
       flexDirection: 'row',
@@ -185,14 +224,38 @@ const styles = StyleSheet.create({
     },
     alarm: {
       position: 'absolute',
-      top: '25%',
+      top: '30%',
       left: '25%',
       overflow: 'hidden',
       height: '180',
-      width: '70%',
+      width: '50%',
       flexDirection: 'row',
+      borderRadius: 20,
       
-      backgroundColor: 'white',
-  },
+      backgroundColor: '#686464ff',
+    },
+      buttons: {
+    borderRadius: 30,
+    width: 60,
+    height: 60,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+    },
+    buttonContainer: {
+      position: 'absolute',
+     
+      alignItems: 'center',
+      
+      top: 500,
+      left: '25%',
+      width: '50%',
+      height: 80,
+      borderRadius: 20,
+      justifyContent: 'space-around',
+      backgroundColor: '#686464ff',
+      
+    },
     
 })
